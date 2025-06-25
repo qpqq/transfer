44a07e636eac4426446864b802beaa3c63a74087
Maxim <nikonov.mo@phystech.edu>
2025-06-25 05:41:02 +0300

    ALLOWED_HOSTS and STATIC_ROOT

--- transfer/settings.py
+++ transfer/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.utils.translation import gettext_lazy as _
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#jgg$h0wiixl2uz4m34&))a+ms3!5p_8$#&h5_kz8v91vk@6rq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['miptransfer.tw1.ru', 'localhost']
 
 # Application definition
 
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
